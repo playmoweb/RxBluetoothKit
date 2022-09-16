4a37509a2a1b6553a2d29ee2926e7aaf2016ee6d
Ghislain <ghislain@playmoweb.com>
2022-09-16 16:08:28 +0200

    Upgrade RxSwift to 6.5.0

--- Source/CentralManagerRestoredState.swift
+++ Source/CentralManagerRestoredState.swift
@@ -60,7 +60,11 @@ public struct CentralManagerRestoredState: CentralManagerRestoredStateType {
         let cbServices = arrayOfAnyObjects.flatMap { $0 as? CBService }
         #endif
 
-        return cbServices.map { Service(peripheral: centralManager.retrievePeripheral(for: $0.peripheral),
-                                        service: $0) }
+        return cbServices.compactMap({ service in
+            if let peripheral = service.peripheral {
+                return Service(peripheral: centralManager.retrievePeripheral(for: peripheral), service: service)
+            }
+            return nil
+        })
     }
 }
