540f554b2e4b4031df38ba06d097e9c2acdb171a
Ghislain <ghislain@playmoweb.com>
2023-09-20 14:45:28 +0200

    chore: fix for Xcode 15

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RxBluetoothKit",
     platforms: [
-        .macOS(.v10_13), .iOS(.v9), .tvOS(.v11), .watchOS(.v4)
+        .macOS(.v10_13), .iOS(.v12), .tvOS(.v11), .watchOS(.v4)
     ],
     products: [
         .library(name: "RxBluetoothKit", targets: ["RxBluetoothKit"])
@@ -16,20 +16,20 @@ let package = Package(
     ],
     targets: [
         .target(
-                name: "RxBluetoothKit",
-                dependencies: [
-                    "RxSwift"
-                ],
-                path: ".",
-                exclude: [
-                    "Example",
-                    "Tests",
-                    "Source/Info.plist",
-                    "Source/RxBluetoothKit.h"
-                ],
-                sources: [
-                    "Source"
-                ]
-            )
+            name: "RxBluetoothKit",
+            dependencies: [
+                "RxSwift"
+            ],
+            path: ".",
+            exclude: [
+                "ExampleApp",
+                "Tests",
+                "Source/Info.plist",
+                "Source/RxBluetoothKit.h"
+            ],
+            sources: [
+                "Source"
+            ]
+        )
     ]
 )
--- RxBluetoothKit.podspec
+++ RxBluetoothKit.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = "RxBluetoothKit"
-  s.version          = "6.0.0"
+  s.version          = "7.0.0"
   s.summary          = "Bluetooth library for RxSwift"
 
   s.description      = <<-DESC
@@ -13,7 +13,7 @@ Pod::Spec.new do |s|
   s.source           = { :git => "https://github.com/polidea/RxBluetoothKit.git", :tag => s.version.to_s }
   s.social_media_url = 'https://twitter.com/polidea'
 
-  s.ios.deployment_target = '9.0'
+  s.ios.deployment_target = '12.0'
   s.osx.deployment_target = '10.13'
   s.watchos.deployment_target = '4.0'
   s.tvos.deployment_target = '11.0'
